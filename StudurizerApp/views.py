2d1361f67382a15d7b8b5e50adce11ba1c880b04
Marvin Hamacher <Xavahus@gmail.com>
2025-04-16 21:52:52 +0200

    9: Terminübersicht ohne Datenmodell und CSS

--- StudurizerApp/views.py
+++ StudurizerApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'homepage.html')
+    events = Event.objects.filter(
+        course__students=request.user,  # oder course__teachers=user
+        end_time__gt=timezone.now()
+    ).order_by('start_time')
+    context = {
+        'events': events
+    }
+    return render(request, 'homepage.html', context)
