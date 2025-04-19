5bcf5b6b2e9ba3f9924f63e8f2843adc9e32c014
Mustafa <104644957+Blacks-Army@users.noreply.github.com>
2025-04-19 17:33:38 +0200

    Allow all Hosts in settings.py

--- StudurizerApp/settings.py
+++ StudurizerApp/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = '$SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '$DOMAIN'
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
