c473b0101c149378ad99314f16d4225ba8d8ab94
Marvin Hamacher <Xavahus@gmail.com>
2025-04-19 12:03:28 +0200

    5: Datenbank optimierung und events view ist fertig

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create_event/', views.CreateEvent.as_view(), name='create_event'),
     path('update_event/<int:id>/', views.UpdateEvent.as_view(), name='update_event'),
     path('delete_event/<int:id>/', views.DeleteEvent.as_view(), name='delete_event'),
+    path('all_events/', views.events, name='all_events'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
-
+from django.utils import timezone
 from events.forms import EventForm
 from events.models import Events
 
@@ -18,9 +19,36 @@ class UpdateEvent(UpdateView):
     template_name = 'courses/update_course.html'
     form_class = EventForm
     success_url = reverse_lazy("home")
-    pk_url_kwarg = 'pk'
 
 
 class DeleteEvent(DeleteView):
     model = Events
     success_url = reverse_lazy('home')
+
+
+def events(request):
+    user_courses_q = Q(course__students=request.user) | Q(course__teachers=request.user)
+    now = timezone.now()
+    events = Events.objects.filter(
+        user_courses_q,
+        end_date__gte=now
+    ).order_by('start_date').select_related('course')
+    archived_main_events = Events.objects.filter(
+        user_courses_q,
+        end_date__lte=now
+    ).order_by('end_date').select_related('course')
+    archived_misc_events = Events.objects.filter(
+        course=None,
+        end_date__lte=now
+    ).order_by('end_date')
+    expired_events = archived_main_events.union(archived_misc_events)
+    misc_events =Events.objects.filter(
+        course=None,
+        end_date__gte=now
+    ).order_by('start_date')
+    context = {
+        'events': events,
+        'expired_events': expired_events,
+        'misc_events': misc_events
+    }
+    return render(request, 'events/eventsdetail.html',context)
