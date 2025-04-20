604696910cf75e119fc4faa224a8546f177654bb
Johann110 <johann.shilinski2002@gmail.com>
2025-04-20 20:11:23 +0200

    added course detail, without events and assignments yet

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from courses import views
 
 urlpatterns = [
+    path('course/<int:pk>/', views.course_detail, name='course_detail'),
     path('create-course/', views.CreateCourse.as_view(), name='create_course'),
     path('update-course/<int:pk>/', views.UpdateCourse.as_view(), name='update_course'),
-    path('delete-course/<int:pk>/', views.DeleteCourse.as_view(), name='course_delete'),
+    path('delete-course/<int:pk>/', views.DeleteCourse.as_view(), name='delete_course'),
 ]
