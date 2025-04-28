dcaa6ae734d96dfd68b396ec2b547d997e22bc83
Marvin Hamacher <Xavahus@gmail.com>
2025-04-28 08:54:10 +0200

    19: Review abgeschlossen

--- accounts/views.py
+++ accounts/views.py
@@ -5,15 +5,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import UserProfile, CustomUser
-from accounts.profile_forms import UserProfileForm
+from accounts.forms import UserProfileForm
 
-@login_required
+#Kein Loginrequired: Machen wir Templateseitig
 def profile_detail(request, id):
-    """
-    View function to display user profile
-    """
     logged_in_user = get_object_or_404(CustomUser, id=id)
-
     try:
         profile = UserProfile.objects.get(user=logged_in_user)
     except UserProfile.DoesNotExist:
@@ -26,13 +22,7 @@ def profile_detail(request, id):
 
     return render(request, "accounts/profile_detail.html", context)
 
-
-@login_required
 def profile_edit(request):
-    """
-    View function to edit user profile
-    """
-    # Get the user's profile or create it if it doesn't exist
     user = request.user
     profile, created = UserProfile.objects.get_or_create(
         user=user,
@@ -55,23 +45,12 @@ def profile_edit(request):
 
     return render(request, 'accounts/profile_edit.html', context)
 
-
-@login_required
 def profile_delete_picture(request):
-    """
-    View to delete the user's profile picture
-    """
     try:
-        # Get the user's profile
         profile = UserProfile.objects.get(user=request.user)
-
-        # Delete the current picture if it exists
         if profile.profile_picture:
-            # Physically delete the file (optional)
             if os.path.isfile(profile.profile_picture.path):
                 os.remove(profile.profile_picture.path)
-
-            # Clear the field in the database
             profile.profile_picture = None
             profile.save()
 
