4780bc229a90f1e7fb02b31b91bebc00c746404b
Mustafa.Atris <swifter_iodize787@getanonym.com>
2025-04-29 14:21:45 +0200

    Gunicorn support testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
+# Django-Initialisierung
+echo "Applying database migrations..."
 python manage.py makemigrations
 python manage.py migrate
+
+echo "Seeding groups..."
 python manage.py seedgroups
-python manage.py runserver 0.0.0.0:8000
+
+# Sammle statische Dateien
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Starte Nginx im Hintergrund
+echo "Starting Nginx..."
+nginx -g "daemon on;"
+
+# Starte Gunicorn
+echo "Starting Gunicorn..."
+gunicorn studurizer.wsgi:application --bind 0.0.0.0:8000
