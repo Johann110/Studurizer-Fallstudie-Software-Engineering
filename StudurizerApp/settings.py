f8c5308e975d30a0a1fd660dc83bab6a195289d1
ADM-JShilinski <jshilinski@zech-management.de>
2025-05-01 21:59:36 +0200

    fixed submission dropzone UI problem with file preview

--- StudurizerApp/settings.py
+++ StudurizerApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 # Basiswerte
 allowed_hosts = ['localhost', '127.0.0.1']
