4b7ddd2bb7261e2e0253383365b9d738713f2885
Marvin Hamacher <Xavahus@gmail.com>
2025-05-03 12:14:48 +0200

    ohne-story: fetch befehle added

--- certificates/views.py
+++ certificates/views.py
@@ -1,6 +1,9 @@
+import json
 import os
 from django.conf import settings
+from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
 from weasyprint import HTML
 from django.core.mail import EmailMessage
 from django.contrib.auth.models import User
@@ -8,36 +11,47 @@ from grades.models import Grade
 from courses.models import Course
 
 
-def create_certficate(request, id, user_ids: list):
-    try:
-        course = Course.objects.prefetch_related('teachers').get(id=id)
-        teacher_text = 'Lehrkräfte: <br>'
-        for teacher in course.teachers.all():
-            teacher_text += f'{teacher.first_name} {teacher.last_name}<br>'
+@csrf_exempt
+def create_certificate(request, id):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            user_ids = data.get('user_ids', [])
 
-        for user_id in user_ids:
-            grade_text = 'Noten:'
-            grade_nr = 1
-            endgrade = 0
-            user = User.objects.get(id=user_id)
-            grades = Grade.objects.filter(
-                submission__user=user,
-                submission__assignment__course_id=course.id
-            )
+            course = Course.objects.prefetch_related('teachers').get(id=id)
+            teacher_text = 'Lehrkräfte: <br>'
+            for teacher in course.teachers.all():
+                teacher_text += f'{teacher.first_name} {teacher.last_name}<br>'
 
-            for grade in grades:
-                endgrade += int(grade.grade)
-                grade_text += f'<p>Note {grade_nr}: {grade.grade}</p><br>'
-                grade_nr += 1
+            for user_id in user_ids:
+                grade_text = 'Noten:'
+                grade_nr = 1
+                endgrade = 0
+                user = User.objects.get(id=user_id)
+                grades = Grade.objects.filter(
+                    submission__user=user,
+                    submission__assignment__course_id=course.id
+                )
 
-            endgrade = round(endgrade / len(grades), 2) if grades else 0
-            grade_text += f'<hr><p>Endnote: {endgrade}</p>'
+                for grade in grades:
+                    endgrade += int(grade.grade)
+                    grade_text += f'<p>Note {grade_nr}: {grade.grade}</p><br>'
+                    grade_nr += 1
 
-            pdf_path = create_pdf(user.first_name, user.last_name, grade_text, teacher_text, course.name)
-            send_email(user.email, pdf_path)
+                endgrade = round(endgrade / len(grades), 2) if grades else 0
+                grade_text += f'<hr><p>Endnote: {endgrade}</p>'
 
-    except Course.DoesNotExist:
-        pass
+                pdf_path = create_pdf(user.first_name, user.last_name, grade_text, teacher_text, course.name)
+                send_email(user.email, pdf_path)
+
+            return JsonResponse({'status': 'success'})
+
+        except Course.DoesNotExist:
+            return JsonResponse({'error': 'Course not found'}, status=404)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 
 def create_pdf(firstname, lastname, gradetext, teachertext, course_name):
