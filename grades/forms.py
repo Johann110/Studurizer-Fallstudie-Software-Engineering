eb8534741961e47d55b0dbc3633a095760ad343b
Marvin Hamacher <Xavahus@gmail.com>
2025-05-12 11:34:04 +0200

    ohne-story: Helpertext

--- grades/forms.py
+++ grades/forms.py
@@ -11,6 +11,7 @@ class GradeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['grade'].help_text = ('Deutsches nummerisches Bewertungssystem. Es sind nur Ganzzahlen erlaubt.')
         for field in self.fields.values():
             existing_classes = field.widget.attrs.get('class', '')
             field.widget.attrs['class'] = f'{existing_classes} form-control'.strip()
